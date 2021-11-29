59c486dad878fa2ab883e5ebdb1ed2641823bb71
Marekmp@outlook.sk <Marekmp@outlook.sk>
2021-11-29 19:39:29 +0100

    mam

--- Open-Lab-02.08/Checker.cs
+++ Open-Lab-02.08/Checker.cs
@@ -6,7 +6,23 @@ namespace Open_Lab_02._08
     {
         public bool IsEmpty(string str)
         {
-            throw new NotImplementedException();
+            string s1 = "abcd";
+            string s2 = "";
+            string s3 = null;
+
+            Console.WriteLine("String s1 {0}.", Test(s1));
+            Console.WriteLine("String s2 {0}.", Test(s2));
+            Console.WriteLine("String s3 {0}.", Test(s3));
+
+            String Test(string s)
+          {
+           if (String.IsNullOrEmpty(s))
+              return "is null or empty";
+            return String.Format("(\"{0}\") is neither null nor empty", s); 
+                else
+    return String.Format("(\"{0}\") is neither null nor empty", s);
+                
+
         }
     }
 }
